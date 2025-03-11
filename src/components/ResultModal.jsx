cd3e6972caabd0a3a04e732e358aea0bc83a16f1
nathenpriyonggo <rpriyo.uic.edu>
2025-03-11 00:14:07 -0500

    Added portals for dialog

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,4 +1,5 @@
 import { forwardRef, useImperativeHandle, useRef } from "react"
+import { createPortal } from "react-dom";
 
 
 const ResultModal = forwardRef(function ResultModal(
@@ -17,7 +18,7 @@ const ResultModal = forwardRef(function ResultModal(
         }
     });
     
-    return (
+    return createPortal(
         <dialog ref={dialog} className="result-modal" onClose={onReset}>
             {userLost && <h2>You Lost</h2>}
             {!userLost && <h2>Your Score: {score}</h2>}
@@ -30,7 +31,8 @@ const ResultModal = forwardRef(function ResultModal(
             <form method="dialog">
                 <button>Close</button>
             </form>
-        </dialog>
+        </dialog>,
+        document.getElementById('modal')
     )
 });
 
